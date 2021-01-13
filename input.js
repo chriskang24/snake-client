f2dfa752585fd259b6d0f656cdcc7ff52553f3db
chriskang24 <christopherjkang24@gmail.com>
2021-01-13 14:54:58 -0500

    user input completed to be called to play.js

--- input.js
+++ input.js
@@ -1,27 +1,49 @@
+let connection; // stores the active TCP connection object
 
+const setupInput = function (conn) {
 
-const setupInput = function() {
+  connection = conn;
+
+  // console.log(conn);
+
   stdin = process.stdin;
 
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  })
+
   stdin.resume();
 
   return stdin;
 }
 
-const handleUserInput = function() {
-  stdin = process.stdin;
+const handleUserInput = function (key) {
 
-  stdin.on('data', (key) => {
+  
+  if (key === '\u0003') {
+    process.exit();
+  }
 
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
+  if (key === "w") {
+    connection.write('Move: up');
+  }
+
+  if (key === "s") {
+    connection.write('Move: down');
+  };
+
+  if (key === "a") {
+    connection.write('Move: left');
+  }
+
+  if (key === "d") {
+    connection.write('Move: right');
+  }
 }
 
 setupInput();
-handleUserInput();
 
 module.exports = setupInput;
