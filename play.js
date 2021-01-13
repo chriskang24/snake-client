7a5e046339b904c5daa5de8e9006c79ec2344449
chriskang24 <christopherjkang24@gmail.com>
2021-01-13 14:51:38 -0500

    completed play.js file calling the client file and user input files

--- play.js
+++ play.js
@@ -4,6 +4,8 @@ const setupInput = require('./input');
 
 console.log('Connecting ...');
 
-connect(); 
+const connection = connect(); 
 
-setupInput();
+// console.log(connection);
+
+setupInput(connection);
