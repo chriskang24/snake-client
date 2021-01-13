cdb4bdc967ea70a28dcceb84262943546980ed77
chriskang24 <christopherjkang24@gmail.com>
2021-01-13 13:36:46 -0500

    user input passed to new input file

--- play.js
+++ play.js
@@ -1,28 +1,9 @@
 const connect = require('./client');
 
-const setupInput = function() {
-  stdin = process.stdin;
-
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  return stdin;
-}
-
-const handleUserInput = function() {
-  stdin = process.stdin;
-
-  stdin.on('data', (key) => {
-
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-}
+const setupInput = require('./input');
 
 console.log('Connecting ...');
 
-setupInput();
-handleUserInput();
 connect(); 
+
+setupInput();
