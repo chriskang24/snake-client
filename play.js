a0deeb288b1117c2dd35678187fddaeb113778df
chriskang24 <christopherjkang24@gmail.com>
2021-01-13 13:25:19 -0500

    added functionality for handling userinput

--- play.js
+++ play.js
@@ -1,5 +1,28 @@
-
 const connect = require('./client');
+
+const setupInput = function() {
+  stdin = process.stdin;
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  return stdin;
+}
+
+const handleUserInput = function() {
+  stdin = process.stdin;
+
+  stdin.on('data', (key) => {
+
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+}
+
 console.log('Connecting ...');
 
-connect();
+setupInput();
+handleUserInput();
+connect(); 
